94c926941ec724e1cc247078435f74dcfac9d646
martijnk1992 <Martijn.Koekkoek2@hva.nl>
2015-08-18 10:04:06 +0200

    + added new textures
    + added new material
    + added new meter script

---
+++ FungyGame/Assets/_Scripts/Meter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meter : MonoBehaviour {
+
+	private GameObject Indicator;
+
+	// Use this for initialization
+	void Start () {
+		Indicator = this.gameObject.transform.GetChild(0).gameObject;
+	}
+
+	public void Forest (float dmg = 2) {
+		Indicator.transform.position = new Vector2(Indicator.transform.position.x - dmg, 
+		                                           Indicator.transform.position.y);
+	}
+
+	public void Fungus (float dmg = 2) {
+		Indicator.transform.position = new Vector2(Indicator.transform.position.x + dmg,
+		                                           Indicator.transform.position.y);
+	}
+
+	void Update () {
+		if(Indicator.transform.position.x <= -100 && Indicator.transform.position.y >= 100) {
+			//GameOver ();
+		}
+		if (Input.GetKeyDown("space")) 
+		{
+			Fungus(2);
+		}
+	}
+}
