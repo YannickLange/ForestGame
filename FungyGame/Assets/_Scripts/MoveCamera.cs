2394c8e9ed4fc2a4325f1f013db05de0201b19eb
Adrian Imboden <mail@adrianimboden.ch>
2015-08-18 09:38:59 +0200

    Added buttons to rotate

--- FungyGame/Assets/_Scripts/MoveCamera.cs
+++ FungyGame/Assets/_Scripts/MoveCamera.cs
@@ -3,6 +3,47 @@ using System.Collections;
 
 public class MoveCamera : MonoBehaviour
 {
+    private bool rotateLeft = false;
+    private bool rotateRight = false;
+    private Vector2 worldStartPoint;
+
+    // convert screen point to world point
+    private Vector2 getWorldPoint(Vector2 screenPoint)
+    {
+        RaycastHit hit;
+        Physics.Raycast(Camera.main.ScreenPointToRay(screenPoint), out hit);
+        return hit.point;
+    }
+    
+    public void StartRotateLeft()
+    {
+        rotateLeft = true;
+    }
+
+    public void StopRotateLeft()
+    {
+        rotateLeft = false;
+    }
+
+    public void StartRotateRight()
+    {
+        rotateRight = true;
+    }
+
+    public void StopRotateRight()
+    {
+        rotateRight = false;
+    }
+
+    public void RotateLeft()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, 30 * Time.deltaTime);
+    }
+
+    public void RotateRight()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, -30 * Time.deltaTime);
+    }
 
     public float cameraSpeed = 1f;
     public float smooth = 2.0F;
@@ -21,15 +62,37 @@ public class MoveCamera : MonoBehaviour
 
 
         //TODO: Change for mobile. Maybe add 2 buttons?
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.Q) || rotateLeft)
         {
-            transform.RotateAround(Vector3.zero, Vector3.up, 30 * Time.deltaTime);
+            RotateLeft();
         }
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKey(KeyCode.E) || rotateRight)
         {
-            transform.RotateAround(Vector3.zero, Vector3.up, -30 * Time.deltaTime);
+            RotateRight();
         }
 
+
+        // only work with one touch
+        if (Input.touchCount == 1)
+        {
+            Touch currentTouch = Input.GetTouch(0);
+            
+            if (currentTouch.phase == TouchPhase.Began)
+            {
+                this.worldStartPoint = this.getWorldPoint(currentTouch.position);
+            }
+            
+            if (currentTouch.phase == TouchPhase.Moved)
+            {
+                Vector2 worldDelta = this.getWorldPoint(currentTouch.position) - this.worldStartPoint;
+                
+                Camera.main.transform.Translate(
+                    -worldDelta.x,
+                    -worldDelta.y,
+                    0
+                );
+            }
+        }
 
     }
 }
