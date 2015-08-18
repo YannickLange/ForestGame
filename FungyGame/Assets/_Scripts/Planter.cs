e0c739a411c663ad62d1a239b620d614aebe2f43
Adrian Imboden <mail@adrianimboden.ch>
2015-08-18 12:14:29 +0200

    Planters: remove unused function

--- FungyGame/Assets/_Scripts/Planter.cs
+++ FungyGame/Assets/_Scripts/Planter.cs
@@ -36,38 +36,6 @@ public class Planter : MonoBehaviour
         return val;
     }
 
-
-    /// <summary>
-    /// Calculate an acceleration with a second order model
-    /// </summary>
-    /// <param name="MaxValue">Max speed of the system</param>
-    /// <param name="delta">Used to calculate the final value. This value is the MaxSpeed - (MaxSpeed * delta) (percentage)</param>
-    /// <param name="wn">Natural frequency</param>
-    /// <param name="z">Damping coefficient</param>
-    /// <param name="tick">Time interval between each point</param>
-    /// <returns>Acceleration curve</returns>
-    public static List<float> CalculateSpeedCurve(float MaxValue, float delta = 0.02f, float wn = 1.5f, float z = 1.1f, float tick = 0.1f)
-    {
-        List<float> speed = new List<float>();
-        if (z == 1.1f)
-            z = (MaxValue * 2f / 100f) + 1.1f;
-        float l1 = -z * wn - wn * Mathf.Sqrt(Mathf.Pow(z, 2) - 1);
-        float l2 = -z * wn + wn * Mathf.Sqrt(Mathf.Pow(z, 2) - 1);
-        float K1 = (MaxValue * l2) / (l2 - l1);
-        float K2 = (MaxValue * l1) / (l2 - l1);
-
-        float t = 0.0f;
-        float deltaMax = MaxValue - (MaxValue * delta);
-        float Ht = 0.0f;
-        do
-        {
-            Ht = K1 * (1 - Mathf.Exp(l1 * t)) - K2 * (1 - Mathf.Exp(l2 * t));
-            speed.Add((Ht * Time.deltaTime) / 4);
-            t += tick;
-        } while (Ht < deltaMax);
-
-        return speed;
-    }
     public void Spawn()
     {
         List<Hexagon> emptyHex = new List<Hexagon>();
